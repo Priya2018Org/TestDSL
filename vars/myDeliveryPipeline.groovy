ddcb287df76aefc7fadcd9b105186565f37d349d
P22601 <32331341+P22601@users.noreply.github.com>
2018-10-11 07:47:50 -0400

    Create myDeliveryPipeline.groovy

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,62 @@
+#!/usr/bin/env groovy
+
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    echo "checkout git"
+}
+}
+
+stage('Echo Variables'){
+            steps {
+                echo "Printing parameters and variables"
+                echo "branch ${branch}"
+                echo "scmUrl: ${scmUrl}"
+                echo "email: ${email}"
+                echo "serverPort: ${8080}"
+                echo "developmentServer: ${developmentServer}"
+                echo "MstagingServer: ${stagingServer}"
+				
+}
+}
+stage('build') {
+    steps {
+        secho "build"
+    }
+}
+
+stage ('test') {
+    steps {
+        echo "test"
+                }
+}
+
+stage('deploy developmentServer'){
+    steps {
+        echo "deploy developmentServer"
+    }
+}
+
+stage('deploy staging'){
+    steps {
+        echo "deploy staging Server"
+    }
+}
+
+stage('deploy production'){
+    steps {
+        echo "production"
+    }
+}
+}
+post {
+    failure {
+        echo "Faliure"
+    }
+}
+}
+}
