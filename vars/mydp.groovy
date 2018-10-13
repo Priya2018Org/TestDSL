22a796c3093546ee76f5e1099a8585bd975bfaec
P22601 <32331341+P22601@users.noreply.github.com>
2018-10-13 15:44:07 -0400

    Update mydp.groovy

--- vars/mydp.groovy
+++ vars/mydp.groovy
@@ -29,7 +29,7 @@ def call(String branch, String scmUrl, String email, String serverPort, String d
                 steps {
                     echo "build"
 			def msbuildpath = "tool name: MSBuild14, type: hudson.plugins.msbuild.MsBuildInstallation"
-			echo ${msbuildpath}
+			echo "${msbuildpath}"
 		//	"'${msbuildpath}'\\msbuild.exe 'C:\\Jenkins\\workspace\\FirstJenkinsBuild\\Webapp1\\webapp1.exe'"
 		}
             }
