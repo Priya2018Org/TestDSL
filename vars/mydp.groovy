3e8b409e1a16bad34f3763075ba54c5d5aa10f30
P22601 <32331341+P22601@users.noreply.github.com>
2018-10-13 14:26:00 -0400

    Update mydp.groovy

--- vars/mydp.groovy
+++ vars/mydp.groovy
@@ -2,63 +2,64 @@
 
 def call() {
     	
-    def call(Map pipelineParams) {
-
     pipeline {
         agent any
         stages {
             stage('checkout git') {
                 steps {
                     echo "checkout git"
-                 }
+                }
             }
 
-stage('Echo Variables'){
-            steps {
-                echo "Printing parameters and variables"
-                echo "branch ${branch}"
-                echo "scmUrl: ${scmUrl}"
-                echo "email: ${email}"
-                echo "serverPort: ${8080}"
-                echo "developmentServer: ${developmentServer}"
-                echo "MstagingServer: ${stagingServer}"
+            stage('Echo Variables'){
+                steps {
+                    echo "Printing parameters and variables"
+                    echo "branch ${branch}"
+                    echo "scmUrl: ${scmUrl}"
+                    echo "email: ${email}"
+                    echo "serverPort: ${8080}"
+                    echo "developmentServer: ${developmentServer}"
+                    echo "MstagingServer: ${stagingServer}"
 				
-}
-}
-stage('build') {
-    steps {
-        secho "build"
-    }
-}
-
-stage ('test') {
-    steps {
-        echo "test"
                 }
-}
+            }
+            
+            stage('build') {
+                steps {
+                    secho "build"
+                }
+            }
 
-stage('deploy developmentServer'){
-    steps {
-        echo "deploy developmentServer"
-    }
-}
+            stage ('test') {
+                steps {
+                    echo "test"
+                }
+            }
 
-stage('deploy staging'){
-    steps {
-        echo "deploy staging Server"
-    }
-}}}
+            stage('deploy developmentServer'){
+                steps {
+                    echo "deploy developmentServer"
+                }
+            }
 
-stage('deploy production'){
-    steps {
-        echo "production"
-    }
-}
-}
-post {
+            stage('deploy staging'){
+                steps {
+                    echo "deploy staging Server"
+                }
+            }
+            
+            stage('deploy production'){
+                steps {
+                    echo "production"
+                }
+            }
+        }//stages
+    }//pipelnine
+
+    post {
     failure {
         echo "Faliure"
     }
-}
-}
+}//post
 
+}
