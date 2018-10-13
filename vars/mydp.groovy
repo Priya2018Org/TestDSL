024fd9c420c41bd0ab607e392c24364f186237c0
P22601 <32331341+P22601@users.noreply.github.com>
2018-10-13 14:36:14 -0400

    Update mydp.groovy

--- vars/mydp.groovy
+++ vars/mydp.groovy
@@ -1,15 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String branch, String scmUrl, String email, String serverPort, String developmentServer, String stagingServer, String productionServer) {
-    	myDeliveryPipeline {
-    branch = 'master'
-    scmUrl = 'ssh://git@myScmServer.com/repos/myRepo.git'
-    email = 'team@example.com'
-    serverPort = '8080'
-    developmentServer = 'dev-myproject.mycompany.com'
-    stagingServer = 'staging-myproject.mycompany.com'
-    productionServer = 'production-myproject.mycompany.com'
-}
+  
     pipeline {
         agent any
         stages {
